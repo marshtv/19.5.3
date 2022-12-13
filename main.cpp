2c1375a0535cf4c063a201839973f273b2acdf9c
marshtv <marshtv.1972@gmail.com>
2022-12-13 23:22:14 +0300

    Start

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main() {
+	std::ifstream payroll;
+	payroll.open("F:\\__C++\\Skillbox\\19\\19.5.3\\Files\\Payroll.txt");
+
+	std::string first_name, first_name_max, last_name, last_name_max, date;
+	int pay, max_pay = 0, sum = 0;
+
+	while (!payroll.eof()) {
+		payroll >> first_name >> last_name >> pay >> date;
+		sum += pay;
+		if (pay > max_pay) {
+			max_pay = pay;
+			first_name_max = first_name;
+			last_name_max = last_name;
+		}
+	}
+	std::cout << 
+	payroll.close();
+	return 0;
+}
